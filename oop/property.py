2a332d400e8b261b8a02cf163dc373cc94ccf1e3
scofieldchen <scofieldchen@163.com>
2019-11-09 08:31:49 +0800

    优化oop property

--- oop/property.py
+++ oop/property.py
@@ -1,29 +1,26 @@
 """
 @property是装饰器，它可以将实例方法转化为对象属性，这是
 在python class中使用setter和getter方法的有效途径.
-
-参考：
-https://www.machinelearningplus.com/python/python-property/
 """
 
 class Person:
 
     def __init__(self, name, age):
         self.name = name
-        self.__age = age  # __age是私有属性，无法在外部被调用
+        self._age = age  # _age是私有属性
 
     def __repr__(self):
         return "Person(name=%s, age=%s)" % (self.name, str(self.age))
 
-    @property  # 将实例方法转换为对象属性, self.age是对象属性
+    @property  # 将实例方法转换为属性
     def age(self):
-        return self.__age
+        return self._age
     
     @age.setter  # 设置对象属性, 格式: @{property_name}.setter
     def age(self, value):  # setter方法与getter方法采用相同的名字，且接受value参数
         if value < 0:
             raise ValueError("age can't be less than 0")
-        self.__age = value
+        self._age = value
 
 
 p = Person(name="alice", age=20)
