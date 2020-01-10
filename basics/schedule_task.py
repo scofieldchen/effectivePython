f817b2b7912160ce263becb6e6d414e7b18155f3
scofieldchen <scofieldchen@163.com>
2020-01-10 16:51:04 +0800

    用schedule运行定时任务

---
+++ basics/schedule_task.py
@@ -0,0 +1,41 @@
+"""
+用schedule运行定时任务
+"""
+
+import schedule
+import time
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+
+def job(num=0):
+    logging.info(f"execute job({num})")
+
+
+# 每隔5秒钟运行一次任务，可设定多个任务
+schedule.every(5).seconds.do(job, num=0)
+schedule.every(5).seconds.do(job, num=1)
+
+# 每隔1分钟运行一次任务
+# schedule.every().minute.do(job)
+
+# 每隔1分钟过15秒运行一次任务
+# schedule.every().minute.at(":15").do(job)
+
+# 每隔1小时过15分钟运行一次任务
+# schedule.every().hour.at(":15").do(job)
+
+# 每天凌晨00:10:00运行一次任务
+# schedule.every().day.at("00:10:00").do(job)
+
+# 每周三下午3点运行一次任务
+# schedule.every().wednesday.at("15:00:00").do(job)
+
+while True:
+    schedule.run_pending()  # 运行所有设定任务
+    time.sleep(1)
