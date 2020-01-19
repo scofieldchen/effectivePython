2a7e6b7a2855189867610013449dbd2fbe7f9f3e
scofieldchen <scofieldchen@163.com>
2020-01-19 11:34:17 +0800

    明确字典运算的步骤

--- algorithm/calculation_on_dict.py
+++ algorithm/calculation_on_dict.py
@@ -1,30 +1,24 @@
 """
-在dict上进行运算，如求最小值，最大值和排序。
+在dict上进行运算: 求最小值，最大值和排序。
 """
 
-# 假设记录货币成交量的字典
-volumes = {
-    "ETH/USDT": 1000,
-    "BTC/USDT": 1200,
-    "XRP/USDT": 500,
-    "EOS/USDT": 400
+# 假设记录股票价格的字典
+prices = {
+    "ETH/USDT": 158.25,
+    "BTC/USDT": 8325.20,
+    "XRP/USDT": 3.254,
+    "EOS/USDT": 8.258
 }
 
-# 如何返回成交量最小的(key, value)对？
+# 关键步骤：先用zip创建包含(values,keys)的可迭代对象，注意值放在第一位
+# 然后调用min(), max(), sorted()函数，它们会根据元组的第一个元素计算
 
-# 常用方法是分别计算最小成交量和对应的货币对，然后组合在一起输出
-min_vol = min(volumes.values())
-symbol = min(volumes, key=lambda k: volumes[k])
-print((symbol, min_vol))
+# 返回值最小的键值对
+print(min(zip(prices.values(), prices.keys())))
 
-# 更有效的方法是利用zip构建(value, key)的迭代对象，再传递给min
-min(zip(volumes.values(), volumes.keys()))
+# 返回值最大的键值对
+print(max(zip(prices.values(), prices.keys())))
 
-# 同理快速找出成交量最大的(key, value)对
-max(zip(volumes.values(), volumes.keys()))
-
-# 按成交量排序
-sorted(zip(volumes.values(), volumes.keys()))
-
-# 也可以指定sorted的key参数，按照成交量进行排序
-sorted(volumes.items(), key=lambda x: x[1])
+# 按值进行排序
+print(sorted(zip(prices.values(), prices.keys())))  # 从小到大
+print(sorted(zip(prices.values(), prices.keys()), reverse=True))  # 从大到小
