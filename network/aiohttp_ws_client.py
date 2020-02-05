4aaaae458af317c49fcd93445cea564c17c49c6f
scofieldchen <scofieldchen@163.com>
2020-02-05 09:14:58 +0800

    修改client的逻辑

--- network/aiohttp_ws_client.py
+++ network/aiohttp_ws_client.py
@@ -11,20 +11,28 @@ import asyncio
 import aiohttp
 import random
 import json
+from pprint import pprint
 
 
 async def main(url):
     """连接ws server，不断发送消息"""
     async with aiohttp.ClientSession() as session:
         async with session.ws_connect(url) as ws:
-            while True:
-                msg = {"symbol": "BTC/USDT", "price": random.randint(1, 100)}
-                await ws.send_bytes(json.dumps(msg).encode("utf8"))
-                await asyncio.sleep(1)
+            # while True:
+                # msg = {"symbol": "BTC/USDT", "price": random.randint(1, 100)}
+                # await ws.send_bytes(json.dumps(msg).encode("utf8"))
+                # await asyncio.sleep(1)
+            async for msg in ws:
+                if msg.type == aiohttp.WSMsgType.ERROR:
+                    break
+                data = json.loads(msg.data.decode("utf8"))
+                # pprint(data)
+                print(data)
 
 
 if __name__ == "__main__":
-    url = "ws://127.0.0.1:8000/ws"
+    # url = "ws://127.0.0.1:8000/ws"
+    url = "ws://47.75.76.103:55558/ws/orderbook"
     loop = asyncio.get_event_loop()
     try:
         loop.run_until_complete(main(url))
