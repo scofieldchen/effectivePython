8f18afcc3cf105c9a95493838a9d094e73357aad
scofieldchen <scofieldchen@163.com>
2020-02-07 09:05:05 +0800

    handle exception with try except

--- concurrency/async/asyncio_exception.py
+++ concurrency/async/asyncio_exception.py
@@ -1,5 +1,8 @@
 """
-异步程序如何处理异常？
+如何在异步程序中处理异常？
+
+1. 使用全局异常处理
+2. 在任务内部使用try except
 """
 
 import asyncio
@@ -13,74 +16,56 @@ logging.basicConfig(
 
 
 ## 主动引发异常
-## 如果不处理异常，系统会提示'Task exception was never retrieved'，程序也会卡住 
-# async def fetch():
-#     """模拟http请求，定时任务"""
+# 如果不处理异常，程序会退出或卡住
+# 如果用loop.run_until_complete()运行任务，遇到异常后会退出
+# 如果用loop.run_forever()运行任务，遇到异常后先抛出'Task exception was never retrieved'，然后卡住 
+# async def main():
 #     while True:
-#         logging.info("Send request")
+#         logging.info("Doing some work")
 #         await asyncio.sleep(random.randint(1, 2))
 #         # 自动引发异常
 #         if random.randint(1, 2) == 2:
-#             raise Exception("Unexpected exception")
-#         logging.info("Get response")
+#             raise Exception("Encounter exception")
 #         await asyncio.sleep(1)
 
+# # asyncio.run(main())
 # loop = asyncio.get_event_loop()
-# try:
-#     loop.create_task(fetch())
-#     loop.run_forever()
-# finally:
-#     loop.close()
+# loop.create_task(main())
+# loop.run_forever()
 
 
 ## 全局异常处理
-## 创建异常处理函数，通过loop.set_exception_handler()添加到事件环
-## 异常处理函数能够捕捉异常，但程序仍然会卡住？任务已经结束？
+# 创建异常处理函数，通过loop.set_exception_handler()添加到事件环
 # def handle_exception(loop, context):
 #     """函数必须接收loop, context两个参数"""
 #     msg = context.get("exception", context["message"])
 #     logging.error("Exception: %s" % msg)
 
-# async def fetch():
-#     """模拟http请求，定时任务"""
+# async def main():
 #     while True:
-#         logging.info("Send request")
+#         logging.info("Doing some work")
 #         await asyncio.sleep(random.randint(1, 2))
-#         # 自动引发异常
 #         if random.randint(1, 2) == 2:
-#             raise Exception("Unexpected exception")
-#         logging.info("Get response")
-#         await asyncio.sleep(1)
+#             raise Exception("Encounter exception")
 
 # loop = asyncio.get_event_loop()
-# loop.set_exception_handler(handle_exception)  # 添加全局异常处理的逻辑
+# loop.set_exception_handler(handle_exception)
 # try:
-#     loop.create_task(fetch())
-#     loop.run_forever()
+#     loop.run_until_complete(main())
 # finally:
 #     loop.close()
 
 
 ## try except
-## 在异步程序中继续使用try except，捕捉异常后程序不会中断，比
-## 全局异常处理函数更加灵活，也更容易理解
-async def fetch():
-    """模拟http请求，定时任务"""
+# 在异步程序中继续使用try except，比全局异常处理函数更加灵活
+async def main():
     while True:
         try:
-            logging.info("Send request")
-            await asyncio.sleep(random.randint(1, 2))
-            # 自动引发异常
+            logging.info("Doing some work")
             if random.randint(1, 2) == 2:
-                raise Exception("Unexpected exception")
-            logging.info("Get response")
-            await asyncio.sleep(1)
+                raise Exception("Encounter exception")
         except Exception as e:
             logging.error(e)
+        await asyncio.sleep(1)
 
-loop = asyncio.get_event_loop()
-try:
-    loop.create_task(fetch())
-    loop.run_forever()
-finally:
-    loop.close()
+asyncio.run(main())
