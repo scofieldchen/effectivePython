7cf6acff083ef33f18d7d13c9a02465db95c2bfe
scofieldchen <scofieldchen@163.com>
2020-10-02 18:59:17 +0800

    优化装饰器函数

--- oop/design_patterns/structure/decorator_func.py
+++ oop/design_patterns/structure/decorator_func.py
@@ -4,46 +4,40 @@
 考虑以下情景，如果要分析函数运行的时长，往往要记录开始运行的时间和结束运行的时间，
 然后计算时间差并记录下来。整个逻辑与函数本身的逻辑没有关联，如果只是为了分析一个
 函数，可以将代码写在函数内部，但如果要分析数十个函数，写在函数内部的方法使代码变得
-非常冗余，这时候就应该把分析时间的代码抽象出来，卸载一个'装饰器函数'中。
+非常冗余，这时候就应该把分析时间的代码抽象出来，放在一个'装饰器函数'中。
 """
 
 import time
+from memory_profiler import memory_usage
+
+import numpy as np
 
 
 def profiling_decorator(f):
-    """装饰器函数，将被装饰的函数作为参数
+    """评估函数运行时间和内存使用的装饰器函数
 
-    1. 在装饰器内部定义新函数，用于封装被装饰的函数，此处是加上分析运行时长的逻辑
+    1. 在装饰器内部定义新函数，用于封装被装饰的函数
     2. 使用*args, **kwargs传递参数
     3. 装饰器最终返回新函数作为结果，供外部调用
     """
     def wrapper(*args, **kwargs):
-        start_time = time.time()
+        t0 = time.time()
         res = f(*args, **kwargs)
-        end_time = time.time()
-        print("Computation time: %.2f seconds" % (end_time - start_time))
+        elapsed = time.time() - t0
+        print(f"time: {elapsed:.3f} seconds")
+        
+        mem, res = memory_usage(
+            (f, args, kwargs), retval=True, timeout=200, interval=1e-7)
+        print(f"ram usage: {max(mem) - min(mem):.4f} mb")
+
         return res
 
     return wrapper
 
 
-#### 使用方法1：直接调用装饰器函数
-# def func():
-#     """被装饰的函数"""
-#     print("doing complicated work...")
-#     time.sleep(3)
-#     return 0
-
-# func = profiling_decorator(func)
-# func()
-
-
-#### 使用方法2：python装饰器
-
 @profiling_decorator
-def func():
-    print("doing complicated work...")
-    time.sleep(3)
-    return 0
+def foo():
+    arr = np.random.randn(10000, 3)
+    return arr * 2
 
-func()
+res = foo()
