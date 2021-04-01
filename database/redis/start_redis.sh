934b54722b32a338e6cdbec8a54253cf5e654480
scofieldchen <scofieldchen@163.com>
2021-04-01 17:54:08 +0800

    start redis with password

--- database/redis/start_redis.sh
+++ database/redis/start_redis.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # 用docker启动redis
-
 docker container run -d --rm --name redis -p 6379:6379 redis
+
+# 如果要加上密码
+# docker container run -d --rm --name redis -p 6379:6379 redis --requirepass 'password'
